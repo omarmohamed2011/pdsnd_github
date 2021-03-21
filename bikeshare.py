bd7dc8083e08c7abdec4f2fb6fccaffb15c663c2
Omar Mohamed <omarmohamedmounir16@gmail.com>
2021-03-21 20:19:43 +0200

    Adding an important user warning

--- bikeshare.py
+++ bikeshare.py
@@ -42,6 +42,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
+    print('Warning! Input data in the convenient format.')
     while True:
         # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
         city = get_choice('\nEnter the city name choosing among (chicago, new york city, washington) and use commas to list more than a name.\n' 
